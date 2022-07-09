0dd77dcfa8c0da6ee8f5e64db7d69bd80289bd0d
Samuel Augusto <saugustocruz@gmail.com>
2022-07-09 02:42:30 -0300

    Adicionando Foto do Perfil

--- config/routes.rb
+++ config/routes.rb
@@ -11,16 +11,18 @@ Rails.application.routes.draw do
     patch 'profile', to: 'profile#update'
   end
   namespace :admins_backoffice do
-    get 'welcome/index' #Dashboard
-    resources :admins #Administradores
-    resources :subjects #Assuntos/Áreas
-    resources :questions #Questões
+    get 'welcome/index' # Dashboard
+    resources :admins   # Administradores
+    resources :subjects # Assuntos/Áreas
+    resources :questions # Perguntas
   end
   
-  devise_for :admins, :skip => [:registrations]
+  devise_for :admins, skip:  [:registrations]
   devise_for :users
-  
+ 
   get 'inicio', to: 'site/welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'backoffice', to: 'admins_backoffice/welcome#index'
+
   root to: 'site/welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
